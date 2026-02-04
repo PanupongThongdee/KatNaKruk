371fc33985016ccb3e3776e519a76a71f64f5e0a
I'm having a bad day . <pranupong.49@gmail.com>
2026-02-05 02:31:50 +0700

    feature

--- kat.java
+++ kat.java
@@ -1,5 +1,12 @@
 public class kat {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+        Example.display();
+    }
+}
+
+class Example {
+    public static void display() {
+        System.out.println("This is an example class.");
     }
 }
